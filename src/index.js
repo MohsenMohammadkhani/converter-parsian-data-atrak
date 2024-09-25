ad40e71c961c9204c2eceee89552aeb6cc85272b
Mohsen MohammadKhani <m.mohammadkhani@Parsian.local>
2024-09-25 10:37:12 +0330

    commit 3

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, HashRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <Router>
-    <App />
-  </Router>
+  <React.StrictMode>
+    {/* <Router> */}
+    <HashRouter>
+      <App />
+    </HashRouter>
+    {/* </Router> */}
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
